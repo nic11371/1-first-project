09c9ba7423c2f4937de28222c2c1e96d34b606d6
Nic11371 <nic11371@gmail.com>
2021-09-30 21:28:14 +0300

    firstProject

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let Footer = () => {
+	return (
+		<div>
+			it.com
+		</div>
+	)
+}
+
+export default Footer
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Profile = () => {
+	return (
+		<div className='profile'>
+			<div>
+				<img src="https://images8.alphacoders.com/790/790793.jpg" alt="" />
+			</div>
+			<div>
+				ava + description
+			</div>
+			<div>
+				My Posts
+				<div>
+					New Post
+				</div>
+				<div>
+					Post1
+				</div>
+				<div>
+					Post2
+				</div>
+			</div>
+
+		</div>
+	)
+}
+
+export default Profile
