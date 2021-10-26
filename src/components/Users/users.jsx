318e2b8d527257efd87348b52da1f0e57f4bc5b6
Nic11371 <nic11371@gmail.com>
2021-10-26 23:09:19 +0300

    54lesson

--- src/components/Users/users.jsx
+++ src/components/Users/users.jsx
@@ -3,37 +3,35 @@ import classes from "./users.module.css"
 import * as axios from "axios"
 import userPhoto from "./../../assets/img/1.png"
 
-const Users = (props) => {
+class Users extends React.Component {
 
-	if (props.users.length === 0) {
+	componentDidMount() {
 		axios.get("https://social-network.samuraijs.com/api/1.0/users")
 			.then(response => {
-				props.setUsers(response.data.items)
-
+				this.props.setUsers(response.data.items)
 			})
 	}
 
-	return <div className={classes.Users}>
-
-		{
-			props.users.map(u => <div key={u.id}>
-				<div>{u.name}</div>
-				<div>
-					<img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" className={classes.userPhoto} />
-				</div>
-				<div>Followed
-					{u.followed ? <button onClick={() => { props.unfollowed(u.id) }} > Unfollow </button>
-						: <button onClick={() => { props.followed(u.id) }}>Follow</button>}
-				</div>
-				<div>{u.status}</div>
-				{/* <div>{u.location.country}
+	render() {
+		return <div className={classes.Users}>
+			{
+				this.props.users.map(u => <div key={u.id}>
+					<div>{u.name}</div>
+					<div>
+						<img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" className={classes.userPhoto} />
+					</div>
+					<div>Followed
+						{u.followed ? <button onClick={() => { this.props.unfollowed(u.id) }} > Unfollow </button>
+							: <button onClick={() => { this.props.followed(u.id) }}>Follow</button>}
+					</div>
+					<div>{u.status}</div>
+					{/* <div>{u.location.country}
 					{u.location.city}</div> */}
+				</div>)
+			}
 
-
-			</div>)
-		}
-
-	</div>
+		</div>
+	}
 }
 
 export default Users
--- src/components/Users/usersContainer.jsx
+++ src/components/Users/usersContainer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { connect } from "react-redux";
 import { followedAC, setUsersAC, unfollowedAC } from "../redux/usersReducer";
 import Users from "./users"
